ac656d1a920d38236852863d27b4456f91be107e
Joel Llacsahuache Copia <joel.llacsahuache@upeu.edu.pe>
2022-09-25 08:35:39 -0500

    Inicializando el layout proncipa

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 from http import server
-from flask import Flask
+from flask import Flask, render_template
 
 appGeneradorMsg= Flask(__name__)
 
-@appGeneradorMsg.route('/')
+@appGeneradorMsg.route('/layout')
 def iniciar():
-    return 'Bienvenido'
+    return render_template('layout.html')
 
 if __name__==('__main__'):
     appGeneradorMsg.run(debug=True)
